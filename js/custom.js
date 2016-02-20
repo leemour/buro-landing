c6f789af18637f33c20fa07b1609e7c9858197b1
nmiloserdov <nmiloserdov09@gmail.com>
2016-02-20 16:44:09 +0300

    add header

--- js/custom.js
+++ js/custom.js
@@ -12,22 +12,20 @@
 $(window).load(function() { // makes sure the whole site is loaded
   "use strict";
 
-  /* ==============================================
-      Menu toggle
-    =============================================== */ 
-    $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 40;
-        if ($(window).scrollTop() > navHeight) {
-            $('.navbar-default').addClass('on');
-        } else {
-            $('.navbar-default').removeClass('on');
-        }
-    });
+    // $(window).bind('scroll', function() {
+    //     var navHeight = $(window).height() - 930;
 
-    $('body').scrollspy({ 
-        target: '.navbar-default',
-        offset: 70
-    })
+    //     if ($(window).scrollTop() > navHeight) {
+    //         $('.navbar-default').addClass('on');
+    //     } else {
+    //         $('.navbar-default').removeClass('on');
+    //     }
+    // });
+
+    // $('body').scrollspy({ 
+    //     target: '.navbar-default',
+    //     offset: 70
+    // })
  
 
 });
@@ -35,7 +33,6 @@ $(window).load(function() { // makes sure the whole site is loaded
   
 
 $(document).ready(function(){
-  
   "use strict";
   
   $("a[data-rel^='prettyPhoto']").prettyPhoto();
@@ -44,67 +41,67 @@ $(document).ready(function(){
        Testimonialcarousel
     =============================================== */ 
 
-  $("#testimonial-carousel").owlCarousel({
+  // $("#testimonial-carousel").owlCarousel({
  
-      navigation : false, // Show next and prev buttons
-      slideSpeed : 600,
-      pagination:true,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3],
-      singleItem:true
+  //     navigation : false, // Show next and prev buttons
+  //     slideSpeed : 600,
+  //     pagination:true,
+  //     itemsDesktop : [1199,3],
+  //     itemsDesktopSmall : [979,3],
+  //     singleItem:true
  
-    });
+  //   });
 
-    // Custom Navigation Events
-    var owl = $("#testimonial-carousel");
+  //   // Custom Navigation Events
+  //   var owl = $("#testimonial-carousel");
 
 
-    // Custom Navigation Events
-    $(".next").click(function(){
-      owl.trigger('owl.next');
-    })
-    $(".prev").click(function(){
-      owl.trigger('owl.prev');
-    })
-    $(".play").click(function(){
-      owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-    })
-    $(".stop").click(function(){
-      owl.trigger('owl.stop');
-    })
+  //   // Custom Navigation Events
+  //   $(".next").click(function(){
+  //     owl.trigger('owl.next');
+  //   })
+  //   $(".prev").click(function(){
+  //     owl.trigger('owl.prev');
+  //   })
+  //   $(".play").click(function(){
+  //     owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+  //   })
+  //   $(".stop").click(function(){
+  //     owl.trigger('owl.stop');
+  //   })
    
 
     /* ==============================================
      Client carousel
     =============================================== */ 
 
- $("#client-carousel").owlCarousel({
-      navigation : false, // Show next and prev buttons
-      slideSpeed : 400,
-      pagination:false,
-      items : 5,
-      rewindNav: true,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3],
-      stopOnHover:true,
-      autoPlay:true
+ // $("#client-carousel").owlCarousel({
+ //      navigation : false, // Show next and prev buttons
+ //      slideSpeed : 400,
+ //      pagination:false,
+ //      items : 5,
+ //      rewindNav: true,
+ //      itemsDesktop : [1199,3],
+ //      itemsDesktopSmall : [979,3],
+ //      stopOnHover:true,
+ //      autoPlay:true
 
-   });
+ //   });
 
 
-  /* ----------------------------------------------------------- */
-  /*  Main slideshow
-  /* ----------------------------------------------------------- */
+  // /* ----------------------------------------------------------- */
+  // /*  Main slideshow
+  // /* ----------------------------------------------------------- */
 
-  $('#slider-carousel').carousel({
-    pause: true,
-    interval: 1000000,
-  });
+  // $('#slider-carousel').carousel({
+  //   pause: true,
+  //   interval: 1000000,
+  // });
 
    
-    $('#app-carousel').carousel({
-        interval: 100000
-    })
+  //   $('#app-carousel').carousel({
+  //       interval: 100000
+  //   })
 
 
 
@@ -128,23 +125,23 @@ $(document).ready(function(){
 Back To Top Button
 =============================================== */  
  
-  $(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
-                $('#back-top').fadeIn();
-            } else {
-                $('#back-top').fadeOut();
-            }
-        });
-      // scroll body to 0px on click
-      $('#back-top').click(function () {
-          $('#back-top a').tooltip('hide');
-          $('body,html').animate({
-              scrollTop: 0
-          }, 800);
-          return false;
-      });
+  // $(window).scroll(function () {
+  //           if ($(this).scrollTop() > 50) {
+  //               $('#back-top').fadeIn();
+  //           } else {
+  //               $('#back-top').fadeOut();
+  //           }
+  //       });
+  //     // scroll body to 0px on click
+  //     $('#back-top').click(function () {
+  //         $('#back-top a').tooltip('hide');
+  //         $('body,html').animate({
+  //             scrollTop: 0
+  //         }, 800);
+  //         return false;
+  //     });
       
-      $('#back-top').tooltip('hide');
+  //     $('#back-top').tooltip('hide');
 
   
 
