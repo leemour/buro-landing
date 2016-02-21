113e7c05427e011529933fac4a4cc85854d52acb
nmiloserdov <nmiloserdov09@gmail.com>
2016-02-21 19:57:35 +0300

    fix forms & footer

--- js/custom.js
+++ js/custom.js
@@ -4,76 +4,27 @@
   * 
   * Template Scripts
   * Created by Themeturn
-
-  
 */
 
-
-$(window).load(function() { // makes sure the whole site is loaded
-  "use strict";
-
-    $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 930;
-
-        if ($(window).scrollTop() > navHeight) {
-            $('.navbar-default').addClass('on');
-        } else {
-            $('.navbar-default').removeClass('on');
-        }
-    });
-
-    $('body').scrollspy({ 
-        target: '.navbar-default',
-        offset: 70
-    })
- 
-
+$(window).bind('scroll', function() {
+    "use strict";
+    var nav_height = $(window).height() - 930;
+    if ($(window).scrollTop() > nav_height) {
+        $('.navbar-default').addClass('on');
+    }
+    
 });
 
-  
+$(window).load(function() { 
+    "use strict";
+    $('.counter').counterUp({
+        delay: 100,
+        time: 500
+    });
+});
 
-$(document).ready(function(){
+$(document).ready(function() {
   "use strict";
-  
-  $("a[data-rel^='prettyPhoto']").prettyPhoto();
-
- /* ==============================================
-       Testimonialcarousel
-    =============================================== */ 
-
-  // $("#testimonial-carousel").owlCarousel({
- 
-  //     navigation : false, // Show next and prev buttons
-  //     slideSpeed : 600,
-  //     pagination:true,
-  //     itemsDesktop : [1199,3],
-  //     itemsDesktopSmall : [979,3],
-  //     singleItem:true
- 
-  //   });
-
-  //   // Custom Navigation Events
-  //   var owl = $("#testimonial-carousel");
-
-
-  //   // Custom Navigation Events
-  //   $(".next").click(function(){
-  //     owl.trigger('owl.next');
-  //   })
-  //   $(".prev").click(function(){
-  //     owl.trigger('owl.prev');
-  //   })
-  //   $(".play").click(function(){
-  //     owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-  //   })
-  //   $(".stop").click(function(){
-  //     owl.trigger('owl.stop');
-  //   })
-   
-
-    /* ==============================================
-     Client carousel
-    =============================================== */ 
 
  $("#client-carousel").owlCarousel({
       navigation : false, // Show next and prev buttons
@@ -87,62 +38,4 @@ $(document).ready(function(){
       autoPlay:true
 
    });
-
-
-  // /* ----------------------------------------------------------- */
-  // /*  Main slideshow
-  // /* ----------------------------------------------------------- */
-
-  // $('#slider-carousel').carousel({
-  //   pause: true,
-  //   interval: 1000000,
-  // });
-
-   
-  //   $('#app-carousel').carousel({
-  //       interval: 100000
-  //   })
-
-
-
-       /* ==============================================
-    Bactstretch js
-=============================================== */
-      $.backstretch([
-          "images/bg/bg1.jpg",
-          "images/bg/bg2.jpg",
-          "images/bg/bg3.jpg"
-        ], {
-            fade: 950,
-            duration: 10000
-        });  
-
-    
- 
-
-
-/* ==============================================
-Back To Top Button
-=============================================== */  
- 
-  // $(window).scroll(function () {
-  //           if ($(this).scrollTop() > 50) {
-  //               $('#back-top').fadeIn();
-  //           } else {
-  //               $('#back-top').fadeOut();
-  //           }
-  //       });
-  //     // scroll body to 0px on click
-  //     $('#back-top').click(function () {
-  //         $('#back-top a').tooltip('hide');
-  //         $('body,html').animate({
-  //             scrollTop: 0
-  //         }, 800);
-  //         return false;
-  //     });
-      
-  //     $('#back-top').tooltip('hide');
-
-  
-
 });
