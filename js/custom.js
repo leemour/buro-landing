f6833990e43c01bdeca8b25c6c3a3a98619e9633
Nikita <nmiloserdov09@gmail.com>
2016-03-02 20:56:27 +0300

    union css/js files, fix bux with form & fix about blocks

--- js/custom.js
+++ js/custom.js
@@ -49,16 +49,23 @@ $(document).ready(function() {
   $('.maps.embed-container').on('click', onMapClickHandler);
   
     
-  $("#client-carousel").owlCarousel({
-      navigation : false, // Show next and prev buttons
-      slideSpeed : 400,
-      pagination:false,
-      items : 5,
-      rewindNav: true,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3],
-      stopOnHover:true,
-      autoPlay:true
+});
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
 
-   });
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
 });
